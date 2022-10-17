fd6cc3af9345d93522d7e221ee4d99f74025fc95
roldanmoncada <roldan.h.moncada@gmail.com>
2022-10-17 14:44:00 -0600

    Updated the README with relevant info. Began to work on JS items. Set two variables. Still looking for best way to do the prompt box portion of things.

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 // Assignment code here
-
+//Created variable of all possible characters for a secure password. Also created empty array variable for the user's input for what type of pw they want to generate.
+var char = "0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var userInput = [];
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
@@ -15,3 +17,8 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
+function generatePassword() {
+  return
+}
